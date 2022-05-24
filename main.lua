64bfc7b6c267847179c38cbf347ea8743c7386c8
Dwij <94942567+IdkDwij@users.noreply.github.com>
2022-05-24 14:25:32 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
-print('trying to make a new language in lua')
-print('\n')
+output('trying to make a new language in lua')
+output('\n')
 local file = io.open(arg[1],"r")
 local code = file:read "*a"
 local filename = arg[1]
 local function execute()
-	code = code:gsub("print","print")
+	code = code:gsub("output","print")
+  code = code:gsub("void","function")
+  code = code:gsub("{}}","end")
   local newfile = io.open(filename:gsub("idk","lua"),"w")
   newfile:write(code)
   newfile:close()

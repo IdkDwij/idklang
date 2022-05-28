ee60b1aad752095b22746158af6df31baa991fcc
Dwij <94942567+IdkDwij@users.noreply.github.com>
2022-05-28 12:38:59 -0400

    trying to make this better

--- main.lua
+++ main.lua
@@ -1,14 +1,14 @@
-output('trying to make a new language in lua')
-output('\n')
+print('trying to make a new language in lua')
+print('\n')
 local file = io.open(arg[1],"r")
 local code = file:read "*a"
 local filename = arg[1]
 local function execute()
-	code = code:gsub("output","print")
-  code = code:gsub("void","function")
+  code = code:gsub("void ","function")
   code = code:gsub("{}}","end")
   local newfile = io.open(filename:gsub("idk","lua"),"w")
-  newfile:write(code)
+  newfile:write('local output = print\n' + code)
+
   newfile:close()
   os.execute("lua test.lua")
 end
